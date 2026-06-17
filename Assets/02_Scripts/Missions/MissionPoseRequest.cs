1ac6e65c8eb93b709ea201237deadcb7ae38443b
KimJeongHwan <jeonghwan916@gmail.com>
2026-06-18 03:30:30 +0900

    feat: 백엔드 데이터 통신을 위한 기본 구조 추가

---
+++ Assets/02_Scripts/Missions/MissionPoseRequest.cs
@@ -0,0 +1,41 @@
+using System;
+
+namespace MdmUnity.Missions
+{
+    [Serializable]
+    public class MissionPoseRequest
+    {
+        public string storyId;
+        public string sceneId;
+        public string missionType;
+        public int captureDurationSec;
+        public int sampleFps;
+        public MissionPoseFrame[] poseFrames;
+    }
+
+    [Serializable]
+    public class MissionPoseFrame
+    {
+        public float timestamp;
+        public MissionPoseLandmarks landmarks;
+    }
+
+    [Serializable]
+    public class MissionPoseLandmarks
+    {
+        public MissionPoseLandmark leftShoulder;
+        public MissionPoseLandmark rightShoulder;
+        public MissionPoseLandmark leftElbow;
+        public MissionPoseLandmark rightElbow;
+        public MissionPoseLandmark leftWrist;
+        public MissionPoseLandmark rightWrist;
+    }
+
+    [Serializable]
+    public class MissionPoseLandmark
+    {
+        public float x;
+        public float y;
+        public float visibility;
+    }
+}
